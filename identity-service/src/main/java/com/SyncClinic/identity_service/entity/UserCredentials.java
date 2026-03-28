c858eb11293372f5ef402d30d48ed3403f6fb8f9
AuthnSaparachchi <authn.sapuarachchi@gmail.com>
2026-03-28 14:29:18 +0530

    create auth service and identity service, user save is corect

---
+++ identity-service/src/main/java/com/SyncClinic/identity_service/entity/UserCredentials.java
@@ -0,0 +1,28 @@
+package com.SyncClinic.identity_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCredentials {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+}
