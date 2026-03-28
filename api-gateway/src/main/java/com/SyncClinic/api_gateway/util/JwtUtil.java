7cf8f2c81f794e45a1401c6b2bd6e63338b78480
AuthnSaparachchi <authn.sapuarachchi@gmail.com>
2026-03-28 11:35:09 +0530

    Authentication setup start

--- api-gateway/src/main/java/com/SyncClinic/api_gateway/util/JwtUtil.java
+++ api-gateway/src/main/java/com/SyncClinic/api_gateway/util/JwtUtil.java
@@ -1,4 +1,27 @@
 package com.SyncClinic.api_gateway.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
 public class JwtUtil {
+
+    // This is a secret key used to digitally sign the tokens.
+    // In a real production app, NEVER hardcode this. It should be in an environment variable.
+    // This string must be at least 256 bits (32 characters) long.
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
