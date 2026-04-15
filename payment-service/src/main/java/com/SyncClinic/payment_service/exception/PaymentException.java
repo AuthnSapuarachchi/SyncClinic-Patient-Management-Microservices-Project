147990ac61894385ca03b6a880950f267adb3d8f
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-04-15 19:36:40 +0530

    Implement PaymentException: extend RuntimeException to handle payment-related errors

--- payment-service/src/main/java/com/SyncClinic/payment_service/exception/PaymentException.java
+++ payment-service/src/main/java/com/SyncClinic/payment_service/exception/PaymentException.java
@@ -1,5 +1,8 @@
 package com.SyncClinic.payment_service.exception;
 
-public class PaymentException {
-    
+public class PaymentException extends RuntimeException {
+    public PaymentException(String message) {
+        super(message);
+    }
 }
+ 
