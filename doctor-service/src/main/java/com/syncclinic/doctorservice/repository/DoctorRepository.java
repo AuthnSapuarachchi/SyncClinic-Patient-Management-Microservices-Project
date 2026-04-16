1d75389cc148ec93fa42ff5ba694034e470bc856
Gayal Thilakarathna <thilakarathnagayal@gmail.com>
2026-04-16 19:28:42 +0530

    telemedicine service integreated

--- doctor-service/src/main/java/com/syncclinic/doctorservice/repository/DoctorRepository.java
+++ doctor-service/src/main/java/com/syncclinic/doctorservice/repository/DoctorRepository.java
@@ -1,11 +1,18 @@
 package com.syncclinic.doctorservice.repository;
 
 import com.syncclinic.doctorservice.model.Doctor;
+import com.syncclinic.doctorservice.model.DoctorStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 // Repository interface for Doctor entity
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+	List<Doctor> findByStatus(DoctorStatus status);
+
+	List<Doctor> findByStatusAndSpecialtyContainingIgnoreCase(DoctorStatus status, String specialty);
 
 }
